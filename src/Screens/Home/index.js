d04debff12669092362292aebd355cf11fe10d54
hidoshino <83619908+hidoshino@users.noreply.github.com>
2021-09-25 17:49:25 -0300

    Configuração Maps

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, {  useState,   useRef   } from "react";
+import { Container } from './styled'
 
+import MapView from 'react-native-maps';
 
 export default () => {
+
+    const map = useRef();
+
+    const [mapLoc, setMapLoc] = useState({
+        center: {
+            latitude: 37.78825,
+            longitude: -122.4324
+        },
+        zoom: 16,
+        pitch: 0,
+        altitude: 0,
+        heading: 0
+    })
+
     return(
-        <SafeAreaView>
-            <Text>sdsddsdssd</Text>
-        </SafeAreaView>
+        <Container>
+            <MapView
+                ref={map}
+                style={{flex: 1}}
+                provider={"google"}
+                camera={mapLoc}
+            ></MapView>
+        </Container>
     )
 }
